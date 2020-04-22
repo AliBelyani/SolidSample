6a0b13e8c67330b28f00e1409db16f61ae6371b3
Ali Belyani <a.belyani88@gmail.com>
2020-04-22 13:44:20 +0430

    add project

---
+++ SolidSample/AbstractSample/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SolidSample.AbstractSample
+{
+    public abstract class Person
+    {
+        public abstract void ShowInfo();
+    }
+
+    public class Teacher : Person
+    {
+        public override void ShowInfo()
+        {
+            Console.WriteLine("This is Teacher");
+        }
+    }
+
+    public class Student : Person
+    {
+        public override void ShowInfo()
+        {
+            Console.WriteLine("This is Student");
+        }
+    }
+}
---
+++ SolidSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using SolidSample.AbstractSample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Teacher();
+            person.ShowInfo();
+
+            person = new Student();
+            person.ShowInfo();
+
+            Console.Read();
+        }
+    }
+}
